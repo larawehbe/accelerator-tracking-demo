e6d80d2f27c29f235f7aed24162f28d56848c6db
larawehbe <larawehbee@outlook.com>
2025-08-24 15:54:14 +0300

    update tests

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -2,8 +2,7 @@
 from fastapi.testclient import TestClient
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from api import app
 
 client = TestClient(app)
